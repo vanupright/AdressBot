0d700e2cb18e00618920fffb255efcc23531918b
Torben <vanupright@googlemail.com>
2018-03-02 12:27:45 +0100

    Init

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=adressbot-Web-Deploy.pubxml -p:Password=mSfomGgA2Hwdqdh4RSdsR6winimKrQou0keo33QSMvSrvwGH9tvh0m4SiS5a
+
